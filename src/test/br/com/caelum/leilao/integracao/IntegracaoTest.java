2dbbed42fb26a77a12a48426c0d9782e0d3170ab
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2012-10-30 10:21:38 -0200

    chrome driver

--- src/test/br/com/caelum/leilao/integracao/IntegracaoTest.java
+++ src/test/br/com/caelum/leilao/integracao/IntegracaoTest.java
@@ -5,17 +5,27 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class IntegracaoTest {
 
 	@Test
 	public void deveLocalizarAMensagemDeBoasVindasDoProjeto(){
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = getDriver();
 		driver.get("http://localhost:7070/pm-75-leiloes/");
 		WebElement mensagaem = driver.findElement(By.id("mensagem"));
 		assertTrue(mensagaem.isDisplayed());
 		assertTrue(mensagaem.getText().equals("Aplicação do curso pm-75-leiloes da Caelum!"));
 		driver.close();
+	}
+
+	private WebDriver getDriver() {
+		String driver = System.getProperty("browser_driver");
+		if (driver.contains("chromedriver")) {
+			System.setProperty("webdriver.chrome.driver", "ChromeDrive/" + driver); 
+			return new ChromeDriver();
+		}
+		return new FirefoxDriver();
 	}
 }
