64de641dcaacf4d2bd591940388d1a35c934bb04
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2012-10-30 16:06:24 -0200

    BoasVindasTest

---
+++ src/test/br/com/caelum/leilao/integracao/BoasVindasTest.java
@@ -0,0 +1,33 @@
+package br.com.caelum.leilao.integracao;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BoasVindasTest extends IntegrationTest {
+
+	private WebDriver driver;
+
+	@Before
+	public void setup() {
+		driver = getDriver();
+	}
+	
+	@Test
+	public void deveLocalizarAMensagemDeBoasVindasDoProjeto(){
+		driver.get("http://localhost:7070/pm-75-leiloes/");
+		WebElement mensagaem = driver.findElement(By.id("mensagem"));
+		assertTrue(mensagaem.isDisplayed());
+		assertTrue(mensagaem.getText().equals("Aplicação do curso pm-75-leiloes da Caelum!"));
+	}
+	
+	@After
+	public void after() {
+		driver.close();
+	}
+
+}
---
+++ src/test/br/com/caelum/leilao/integracao/IntegrationTest.java
@@ -0,0 +1,19 @@
+package br.com.caelum.leilao.integracao;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public abstract class IntegrationTest {
+
+	protected WebDriver getDriver() {
+		String driver = System.getProperty("browser_driver");
+		if (driver != null && driver.contains("chromedriver")) {
+			System.setProperty("webdriver.chrome.driver", "ChromeDrive/"
+					+ driver);
+			return new ChromeDriver();
+		}
+		return new FirefoxDriver();
+	}
+
+}
