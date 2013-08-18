75b09d2fdf2e0ead4c4de960947c5b87a4827b9e
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2013-08-18 19:25:45 -0300

    update Avaliador.java to correct

--- src/main/br/com/caelum/leilao/servico/Avaliador.java
+++ src/main/br/com/caelum/leilao/servico/Avaliador.java
@@ -5,7 +5,7 @@ import br.com.caelum.leilao.dominio.Leilao;
 
 class Avaliador {   
 
-	private double maiorDeTodos = Douwble.NEGATIVE_INFINITY; 
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY; 
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
 	private double media = 0;
 
