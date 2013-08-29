cb907dec47df45d9029800cfb4eeeea5f717241c
Nico Steppat <nico.steppat@caelum.com.br>
2013-08-29 13:27:08 -0300

    Removendo import do TakeScreenShot

--- src/test/br/com/caelum/leilao/integracao/BoasVindasTest.java
+++ src/test/br/com/caelum/leilao/integracao/BoasVindasTest.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.internal.selenesedriver.TakeScreenshot;
 
 public class BoasVindasTest extends IntegrationTest {
 
